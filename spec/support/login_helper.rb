bc4ec19cef3eca6f2e1c2a113b5ffdc994ee6be9
Johann Kerr <johannkerr@users.noreply.github.com>
2017-06-15 11:55:20 -0400

    Create login_helper.rb

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -12,6 +12,8 @@ module LoginHelper
     fill_in("user[nausea]", :with => "2")
     fill_in("user[tickets]", :with => "15")
     fill_in("user[password]", :with => "password")
+    fill_in("user[password_confirmation]", :with => "password")
+    
     click_button('Create User')
   end
 
