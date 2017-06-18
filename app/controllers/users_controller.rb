ff6319d6627506059730427653641696c98a1ab0
Jason Sears <jsears4585@gmail.com>
2017-06-18 19:07:23 -0400

    Added sign-in view and User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = find_user
+  end
+
+  def edit
+    @user = find_user
+  end
+
+  def update
+    @user = find_user
+    if @user.update?
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find_user.delete
+    redirect_to :root
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+    end
+
+    def find_user
+      User.find(params[:id])
+    end
+
 end
